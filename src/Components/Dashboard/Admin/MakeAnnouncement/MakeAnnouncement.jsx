6999c0899a2a67ca4fe3419f51ffdb94ee4e255a
Muhammad Saif <muhammadsaif7717@gmail.com>
2024-06-09 05:55:40 +0600

    Announcement Posting System Added

--- src/Components/Dashboard/Admin/MakeAnnouncement/MakeAnnouncement.jsx
+++ src/Components/Dashboard/Admin/MakeAnnouncement/MakeAnnouncement.jsx
@@ -1,9 +1,69 @@
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
 
 
 const MakeAnnouncement = () => {
+    const axiosPublic = useAxiosPublic();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const description = form.description.value;
+        const announcement = {
+            title: title,
+            description: description,
+        }
+
+        //post to DB
+        axiosPublic.post('/announcements', announcement)
+            .then(res => {
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "User Created Successfully",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    form.reset();
+                }
+            })
+    };
+
     return (
-        <div>
-            MakeAnnouncement
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="w-2/3 bg-base-200 shadow-xl p-10 border">
+                <h1 className="text-4xl font-semibold text-center mb-5">Make an Announcement</h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label className="block text-lg font-medium">Title</label>
+                        <input
+                            type="text"
+                            name='title'
+                            className="w-full p-2 border rounded"
+                            placeholder="title..."
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-lg font-medium">Description</label>
+                        <textarea
+                            name='description'
+                            className="w-full p-2 border rounded"
+                            rows="5"
+                            placeholder="description..."
+                            required
+                        />
+                    </div>
+                    <div className="flex justify-center">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
