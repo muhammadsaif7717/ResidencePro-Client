be660722c46d886d6fe8f44682851442974dffa7
Muhammad Saif <muhammadsaif7717@gmail.com>
2024-06-09 06:02:28 +0600

    Announcement Viewing functionality Added

--- src/Components/Dashboard/User&Member/Announcements/Announcements.jsx
+++ src/Components/Dashboard/User&Member/Announcements/Announcements.jsx
@@ -1,7 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
+
 const Announcements = () => {
+    const axiosPublic = useAxiosPublic();
+    const { data: announcements = [], isLoading } = useQuery({
+        queryKey: ['announcements'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/announcements');
+            return res.data;
+        }
+    });
+
+    if (isLoading) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <span className="loading loading-bars loading-lg scale-110"></span>
+            </div>
+        );
+    }
     return (
-        <div>
-            Announcements
+        <div className="p-5">
+            <h1 className="text-4xl font-semibold text-center mb-5">Announcements</h1>
+            <div className="overflow-x-auto">
+                <table className="table-auto w-full border-collapse border border-gray-300">
+                    <thead>
+                        <tr className="bg-gray-200">
+                            <th className="border border-gray-300 p-2">Title</th>
+                            <th className="border border-gray-300 p-2">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {announcements.map((announcement) => (
+                            <tr key={announcement._id} className="hover:bg-gray-100">
+                                <td className="border border-gray-300 p-2">{announcement.title}</td>
+                                <td className="border border-gray-300 p-2">{announcement.description}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
